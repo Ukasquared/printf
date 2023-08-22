01b15fbe33d05a2bbf6f7e87ab675cd1ae5555ea
Hamdyyat <alhassanhamdy1@gmail.com>
2023-08-22 14:48:44 -0700

    get_spec

--- getspec.c
+++ getspec.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
 * get_spec - get the function of the corresponding character
-* 
-*@c: character
 *
+* @c: character
+* Return: pointer to function
 */
 
-void (*get_spec(char c))(va_list arg)
+int (*get_spec(char c))(va_list arg)
 {
 	conversion specifier[] = {
+		{'i', prints_integer},
 		{'c', prints_character},
 		{'d', prints_integer},
 		{'s', prints_string}
 	};
 	int i = 0;
 
-	while (i < 3)
+	while (i < 4)
 	{
 		if (c == specifier[i].c)
 			return (specifier[i].va_arg_func);
