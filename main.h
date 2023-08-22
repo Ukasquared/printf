55797695c7c2394730de40638b94e8a4b6c5cd32
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-22 05:19:40 +0100

    header file

--- main.h
+++ main.h
@@ -7,25 +7,26 @@
 int _strlen(char *);
 int _strcmp(char *, char *);
 int _printf(const char *format, ...);
-void (*get_spec(char c))(va_list arg);
-void prints_string(va_list arg);
-void prints_character(va_list arg);
-void prints_integer(va_list arg);
+int (*get_spec(char c))(va_list arg);
+int prints_string(va_list arg);
+int prints_character(va_list arg);
+int prints_integer(va_list arg);
+void reverse(char str[], int len);
+char *_itoa(int num, char *str, int base);
 
 /**
 * struct _printf - converts according to conversio specifier
 * @c: character
 * @va_arg_func: function pointer
 */
-struct _printf {
+struct _printf
+{
 	char c;
-	void (*va_arg_func)(va_list);
+	int (*va_arg_func)(va_list);
 };
-
 /**
 * conversion - modified struct print_f
 */
-
 typedef struct _printf conversion;
 
 #endif
