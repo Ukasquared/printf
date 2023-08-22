43d7a969b5146ae19dac4a2f3a1fe6c43c7bba43
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-22 05:22:05 +0100

    handles the specifier function

--- specifierfunc.c
+++ specifierfunc.c
@@ -4,36 +4,46 @@
 * prints_string - prints integer value to standard output
 *
 * @arg: takes in the macro va_list argument
+* Return: bytes of character
 */
 
-void prints_string(va_list arg)
+int prints_string(va_list arg)
 {
 	int len_byte;
 	char *value = va_arg(arg, char *);
+
 	len_byte = _strlen(value) * sizeof(char);
-	write(1, &value, len_byte);
+	return (write(1, value, len_byte));
 }
 
 /**
 * prints_character - prints integer value to standard output
 *
 * @arg: takes in the macro va_list argument
+* Return: bytes of character
 */
 
-void prints_character(va_list arg)
+int prints_character(va_list arg)
 {
 	char value = va_arg(arg, int);
-	write(1, &value, sizeof(char));
+
+	return (write(1, &value, sizeof(char)));
 }
 
 /**
 * prints_integer - prints integer value to standard output
 *
 * @arg: takes in the macro va_list argument
+* Return: bytes of character
 */
 
-void prints_integer(va_list arg)
+int prints_integer(va_list arg)
 {
+	char str[100];
+	int len_bytes;
 	int value = va_arg(arg, int);
-	write(1, &value, sizeof(int));
+	char *string_int = _itoa(value, str, 10);
+
+	len_bytes = _strlen(string_int);
+	return (write(1, string_int, len_bytes));
 }
